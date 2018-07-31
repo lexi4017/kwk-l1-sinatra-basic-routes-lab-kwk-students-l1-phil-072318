5933c7503dd7cdfecefbd60b866c1a62209275e3
Alexsia Light <elightl729@comcast.net>
2018-07-31 17:05:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  #home page ...what ever the website name is 
+  get '/' do 
+    "Hello, World!"
+end 
+  #localhost/name 
+  get '/name' do 
+    "My name is Lexi"
+   end
+
+
 end
